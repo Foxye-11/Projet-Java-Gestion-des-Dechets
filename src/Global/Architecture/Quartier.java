48e3b6c66df84a0b3f78adad71fc1be72e191b37
Bourdbou <dimitri.tsatsaris@icloud.com>
2025-12-06 14:08:57 +0100

    mise en place de la coloration des quartiers

--- src/Global/Architecture/Quartier.java
+++ src/Global/Architecture/Quartier.java
@@ -3,13 +3,40 @@ package Global.Architecture;
 import java.util.LinkedList;
 
 public class Quartier {
+
     private String nom;
     private LinkedList<Rue> rues;
     private boolean[][][] occupation = new boolean[12][5][7];
+    private LinkedList<Quartier> voisins = new LinkedList<>();
+    private int couleur = -1;
 
     public Quartier (String nom){
         this.nom = nom;
         this.rues = new LinkedList<>();
+    }
+
+    public static void colorierQuartiers(LinkedList<Quartier> quartiers){
+        for(Quartier q : quartiers){
+            boolean[] interdit = new boolean[quartiers.size()];
+
+            for (Quartier v : q.getVoisins()){
+                int c = v.getCouleur();
+                if(c != -1){
+                    interdit[c] = true;
+                }
+            }
+
+            int couleur = 0;
+            while(couleur < interdit.length && interdit[couleur]) couleur++;
+            q.setCouleur(couleur);
+        }
+    }
+
+    public void addVoisin(Quartier q){
+        if (q != this && !voisins.contains(q)){
+            voisins.add(q);
+            q.voisins.add(this);
+        }
     }
 
     public void addRue(Rue r){if (!rues.contains(r)) rues.add(r);}
@@ -17,5 +44,9 @@ public class Quartier {
     public LinkedList<Rue> getRues(){return rues;}
     public boolean estLibre(int m, int s, int j){return !occupation[m][s][j];}
     public void occuper(int m, int s, int j){occupation[m][s][j] = true;}
+    public LinkedList<Quartier> getVoisins(){return voisins;}
+    public int getCouleur(){return couleur;}
+    public void setCouleur(int c){this.couleur = c;}
+
 
 }
